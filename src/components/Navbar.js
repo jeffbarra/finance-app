6162a4683d6fe3edc81c4fcda4e70c19a63c4d26
Jeff Barra <103481560+jeffbarra@users.noreply.github.com>
2023-02-25 17:33:59 -0700

    Add files via upload

---
+++ src/components/Navbar.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { Twirl as Hamburger } from "hamburger-react";
+import logo from "../images/logo.png";
+import "./Navbar.css";
+
+const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+
+  const closeMenu = () => setClick(false);
+
+  return (
+    <div className="header">
+      <nav className="navbar">
+        <a href="/" className="logo">
+          <img src={logo} alt="logo" />
+        </a>
+        <div className="hamburger" onClick={handleClick}>
+          <Hamburger />
+        </div>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <a href="/" onClick={closeMenu}>
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#about" onClick={closeMenu}>
+              About
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#testimonials" onClick={closeMenu}>
+              Testimonials
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="#demo" onClick={closeMenu}>
+              Demo
+            </a>
+          </li>
+          <button className="navbar-btn">Sign In</button>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
